f34a0aad457c1648cc4f183fe1a339bb861b50ca
niraj <steve64725@gmail.com>
2025-05-31 12:28:12 +0000

    routing fic=xed

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { resolve } from 'path';
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,6 +13,14 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    {
+      name: 'copy-index-to-404',
+      closeBundle: () => {
+        const indexPath = resolve(__dirname, 'dist/index.html');
+        const notFoundPath = resolve(__dirname, 'dist/404.html');
+        fs.copyFileSync(indexPath, notFoundPath);
+      }
+    },
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
